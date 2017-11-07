a86212a7eaeebba1fdf86eea0a3ceda72ba9eb80
Victor Fellipe <victorfgcabeceira@gmail.com>
2017-11-07 21:59:52 -0200

    Changing forums view page and creating admin modules for forum

--- simplemooc/forum/admin.py
+++ simplemooc/forum/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Thread, Reply
+
+
+class ThreadAdmin(admin.ModelAdmin):
+
+  list_display = ['title', 'author', 'created_at', 'updated_at']
+  search_fields = ['title', 'author__email', 'body']
+
+class ReplyAdmin(admin.ModelAdmin):
+
+  list_display = ['thread', 'author', 'created_at', 'updated_at']
+  search_fields = ['thread__title', 'author__email', 'reply']
+
+
+admin.site.register(Thread, ThreadAdmin)
+admin.site.register(Reply, ReplyAdmin)
--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
-class ForumView(TemplateView):
+from .models import Thread
+# class ForumView(TemplateView):
+#
+#   template_name = 'forum/index.html'
+class ForumView(ListView):
 
+  model = Thread
+  paginate_by = 10
   template_name = 'forum/index.html'
 
 index = ForumView.as_view()
