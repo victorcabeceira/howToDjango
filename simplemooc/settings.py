1c720e008bab911dbc284b69aebc5d1b3dccb6a2
Victor Fellipe <victorfgcabeceira@gmail.com>
2017-11-07 22:08:56 -0200

    Updating allowed_hosts

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -29,7 +29,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
   '127.0.0.1',
-  'testserver']
+  'testserver',
+  'sm-victor.herokuapp.com']
 
 
 # Application definition
@@ -49,7 +50,7 @@ INSTALLED_APPS = [
     'simplemooc.core',
     'simplemooc.accounts',
     'simplemooc.courses',
-    'simplemooc.forum',
+    'simplemooc.forum'
 ]
 
 MIDDLEWARE = [
@@ -165,7 +166,7 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
